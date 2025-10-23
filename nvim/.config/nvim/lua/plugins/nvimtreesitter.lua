1f27ffd4a1b9d2c98cd6302c65fac4a2eb32df63
Evan Krueger <evank0301@gmail.com>
2025-10-22 20:26:34 -0500

    fix neotree

--- nvim/.config/nvim/lua/plugins/nvimtreesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvimtreesitter.lua
@@ -1,1 +1,10 @@
-return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+return {
+  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
